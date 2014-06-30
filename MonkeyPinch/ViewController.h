83ebe02ffb3cdd20c66ab3ac78e1766287cca3e7
Jesus Magana <maganazero5@gmail.com>
2014-06-29 21:51:29 -0700

    Initial Commit

---
+++ MonkeyPinch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MonkeyPinch
+//
+//  Created by Jesus Magana on 6/29/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MonkeyPinchTests/MonkeyPinchTests.m
@@ -0,0 +1,34 @@
+//
+//  MonkeyPinchTests.m
+//  MonkeyPinchTests
+//
+//  Created by Jesus Magana on 6/29/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MonkeyPinchTests : XCTestCase
+
+@end
+
+@implementation MonkeyPinchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
